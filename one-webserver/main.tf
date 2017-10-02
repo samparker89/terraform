292de883088e74856805aef704df9f504e1fd360
Sam Parker <developer73@Developer73s-MacBook-Pro.local>
2017-10-02 15:26:34 +0100

    Moved terraform web server code to own directory

--- one-webserver/main.tf
+++ one-webserver/main.tf
@@ -1,16 +1,21 @@
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    default = "8080"
+}
+
 provider "aws" {
     region = "us-east-2"
 }
 
 resource "aws_instance" "sams-example" {
-    ami = "ami-3c715059"
+    ami = "ami-a7aa8ac2"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["${aws_security_group.sams-security-group.id}"]
 
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p "${var.server_port}" &
                 EOF
 
 
@@ -23,9 +28,13 @@ resource "aws_security_group" "sams-security-group" {
     name = "sams-security-group"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = "${var.server_port}"
+        to_port = "${var.server_port}"
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+}
+
+output "sams-example_public_ip" {
+    value = "${aws_instance.sams-example.public_ip}"
 }
