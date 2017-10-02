5e0c30a450624df7f12fa8e4bb3f4e8f68f5d9f6
Sam Parker <developer73@Developer73s-MacBook-Pro.local>
2017-10-02 13:39:49 +0100

    Added bash script to create dummy web server

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ resource "aws_instance" "sams-example" {
     ami = "ami-3c715059"
     instance_type = "t2.micro"
 
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
     tags {
         Name = "sams-terraform-example"
     }
