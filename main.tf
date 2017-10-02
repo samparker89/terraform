c46fe422712bd4d503fe4b3b23b566b8c9c3543d
Sam Parker <developer73@Developer73s-MacBook-Pro.local>
2017-10-02 13:46:19 +0100

    Added security sams-security-group id to sams-example aws_instance

--- main.tf
+++ main.tf
@@ -5,12 +5,14 @@ provider "aws" {
 resource "aws_instance" "sams-example" {
     ami = "ami-3c715059"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["${aws_security_group.sams-security-group.id}"]
 
     user_data = <<-EOF
                 #!/bin/bash
                 echo "Hello, World" > index.html
                 nohup busybox httpd -f -p 8080 &
                 EOF
+
 
     tags {
         Name = "sams-terraform-example"
